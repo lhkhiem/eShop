5ed6f2c5e11f7254e42e207a7d3b151de6b2dc46
LeKhiem <LeKhiem@LEHOANGKHIEM>
2020-07-26 21:09:14 +0700

    tao database

---
+++ eShop.Data/EF/EShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Produts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
