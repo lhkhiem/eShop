1e292c0f7d6ad131b5e514f636b5e172a6dcd05a
LeKhiem <LeKhiem@LEHOANGKHIEM>
2020-07-29 21:52:14 +0700

    tạo dữ liệu mẫu cho database

--- eShop.Data/Entities/Product.cs
+++ eShop.Data/Entities/Product.cs
@@ -13,7 +13,6 @@ namespace eShop.Data.Entities
         public int Stock { set; get; }
         public int ViewCount { set; get; }
         public DateTime DateCreated { set; get; }
-        public string SeoAlias { set; get; }
 
         public List<ProductInCategory> ProductInCategories { get; set; }
 
