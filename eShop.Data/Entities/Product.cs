93728652dc4ed42773b181542b913ede3676d236
LeKhiem <LeKhiem@LEHOANGKHIEM>
2020-08-02 23:16:14 +0700

    Thêm mới bảng ProductImage bằng migration

--- eShop.Data/Entities/Product.cs
+++ eShop.Data/Entities/Product.cs
@@ -13,14 +13,11 @@ namespace eShop.Data.Entities
         public int Stock { set; get; }
         public int ViewCount { set; get; }
         public DateTime DateCreated { set; get; }
-
         public List<ProductInCategory> ProductInCategories { get; set; }
-
         public List<OrderDetail> OrderDetails { get; set; }
-
         public List<Cart> Carts { get; set; }
-
         public List<ProductTranslation> ProductTranslations { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
 
     }
 }
