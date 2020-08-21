776718301c5c1e22949b08dc633b7d528ba9f7ce
LeKhiem <LeKhiem@LEHOANGKHIEM>
2020-08-21 23:37:39 +0700

    Create AdminApp LoginView wwoking with API

---
+++ eShop.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShop.AdminApp.Services;
+using eShop.ViewModels.System.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserApiClient _userApiClient;
+
+        public UserController(IUserApiClient userApiClient)
+        {
+            _userApiClient = userApiClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            if (ModelState.IsValid)
+                return View(ModelState);
+            var token = await _userApiClient.Authenticate(request);
+            return View(token);
+        }
+    }
+}
