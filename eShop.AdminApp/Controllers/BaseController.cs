781f1eaddf88d66ef3f6600304a76a3b9a406d97
LeKhiem <LeKhiem@LEHOANGKHIEM>
2020-11-24 22:25:54 +0700

    Tách Login ra khỏi User, tạo thêm BaseController để check session mỗi lần truy cập Action, bỏ View(ModelState) -> View() để bắt lỗi trên View

---
+++ eShop.AdminApp/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace eShop.AdminApp.Controllers
+{
+    public class BaseController : Controller
+    {
+        //Kiểm tra session có tồn tại hay không. Phương thức này đc gọi đầu tiên trong Controller
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var sessions = context.HttpContext.Session.GetString("Token");
+            if (String.IsNullOrEmpty(sessions))
+            {
+                context.Result = new RedirectToActionResult("Index", "Login", null);
+            }
+            base.OnActionExecuted(context);
+        }
+    }
+}
